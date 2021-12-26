3cf21d365b4d14ab550e323eda5c87c50b937510
GalvinGao <me@galvingao.com>
2021-12-26 20:19:40 +0800

    fix: test failure

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -12,6 +12,8 @@ func ConfigureLogger() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
+	_ = os.Mkdir("logs", os.ModePerm)
+
 	logFile, err := os.OpenFile("logs/app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Panic().Err(err).Msg("Failed to open log file")
