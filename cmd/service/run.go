d186cf8df63eb71ec79e4a67d05145aebf6bdf10
GalvinGao <me@galvingao.com>
2022-02-17 22:17:03 +0800

    feat: separate listen & http serving

--- cmd/service/run.go
+++ cmd/service/run.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"context"
-	"time"
+	"net"
 
 	"go.uber.org/fx"
 	// "github.com/davecgh/go-spew/spew"
@@ -14,20 +14,16 @@ import (
 func run(app *fiber.App, config *config.Config, lc fx.Lifecycle) {
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
-			errChan := make(chan error)
+			ln, err := net.Listen("tcp", config.Address)
+			if err != nil {
+				return err
+			}
 
 			go func() {
-				errChan <- app.Listen(config.Address)
+				app.Listener(ln)
 			}()
 
-			// wait for at maximum 100ms for errChan to return anything, else just assume succeeded and return
-			// nil for error
-			select {
-			case err := <-errChan:
-				return err
-			case <-time.After(100 * time.Millisecond):
-				return nil
-			}
+			return nil
 		},
 		OnStop: func(ctx context.Context) error {
 			if config.DevMode {
