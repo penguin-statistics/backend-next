522197ac37b1c1df5d96af6884a59a01e1a5aa89
GalvinGao <me@galvingao.com>
2022-02-27 19:23:57 +0800

    fix: admin save

--- internal/controllers/admin.go
+++ internal/controllers/admin.go
@@ -27,9 +27,6 @@ type AdminController struct {
 func RegisterAdminController(admin *server.Admin, c AdminController) {
 	admin.Post("/render/event", c.UpdateBrandNewEvent)
 	admin.Post("/save", c.SaveRenderedObjects)
-	admin.Post("/intentionally/panic", func(c *fiber.Ctx) error {
-		panic("intentional panic")
-	})
 }
 
 func (c *AdminController) UpdateBrandNewEvent(ctx *fiber.Ctx) error {
