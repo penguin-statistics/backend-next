4181575f9e6d270becf82c4c17d846aa38a5fecf
GalvinGao <me@galvingao.com>
2022-02-27 20:07:15 +0800

    fix: use null.X instead of *null.X

--- internal/models/zone.go
+++ internal/models/zone.go
@@ -17,11 +17,11 @@ type Zone struct {
 	// Category of the zone.
 	Category string `json:"category" example:"MAINLINE"`
 	// Type of the zone, e.g. "AWAKENING_HOUR" or "VISION_SHATTER". Optional and only occurres when `category` is "MAINLINE".
-	Type *null.String `json:"type,omitempty" swaggertype:"string" example:"AWAKENING_HOUR"`
+	Type null.String `json:"type,omitempty" swaggertype:"string" example:"AWAKENING_HOUR"`
 	// Name is a map with language code as key and the name of the item in that language as value.
 	Name json.RawMessage `json:"name"`
 	// Existence is a map with server code as key and the existence of the item in that server as value.
 	Existence json.RawMessage `json:"existence"`
 	// Background is the path of the background image of the zone, relative to the CDN endpoint.
-	Background *null.String `json:"background,omitempty" swaggertype:"string"`
+	Background null.String `json:"background,omitempty" swaggertype:"string"`
 }
