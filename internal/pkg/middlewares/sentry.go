ec57f670f2fc38726a665c4b4894302019ad6de3
GalvinGao <me@galvingao.com>
2022-03-06 02:09:19 +0800

    feat: add sentry tracing

--- internal/pkg/middlewares/sentry.go
+++ internal/pkg/middlewares/sentry.go
@@ -1,8 +1,12 @@
 package middlewares
 
 import (
+	"net/http"
+
+	"github.com/getsentry/sentry-go"
 	"github.com/gofiber/contrib/fibersentry"
 	"github.com/gofiber/fiber/v2"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
 
 	"github.com/penguin-statistics/backend-next/internal/constants"
 )
@@ -12,6 +16,14 @@ func EnrichSentry() func(c *fiber.Ctx) error {
 		if hub := fibersentry.GetHubFromContext(c); hub != nil {
 			hub.Scope().SetTag("request_id", c.Locals(constants.ContextKeyRequestID).(string))
 		}
+
+		var r http.Request
+		if err := fasthttpadaptor.ConvertRequest(c.Context(), &r, true); err != nil {
+			return err
+		}
+		rootSpan := sentry.StartSpan(c.Context(), "backend", sentry.ContinueFromRequest(&r))
+		defer rootSpan.Finish()
+
 		return c.Next()
 	}
 }
