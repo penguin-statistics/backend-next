265da83ce0b5a7e8a758cb0187a22169c5559a2a
AlvISsReimu <alvissreimu@gmail.com>
2022-03-06 17:09:52 +0800

    fix: refactor item quantity and type amount check

--- internal/utils/reportutils/verify_drops.go
+++ internal/utils/reportutils/verify_drops.go
@@ -3,7 +3,6 @@ package reportutils
 import (
 	"context"
 	"fmt"
-	"strconv"
 
 	"github.com/ahmetb/go-linq/v3"
 	"github.com/pkg/errors"
@@ -26,32 +25,12 @@ func NewDropVerifier(dropInfoRepo *repos.DropInfoRepo) *DropVerifier {
 }
 
 func (d *DropVerifier) Verify(ctx context.Context, report *types.SingleReport, reportTask *types.ReportTask) error {
-	drops := report.Drops
-	tuples := make([][]string, 0, len(drops))
-	var err error
-	linq.From(drops).
-		SelectT(func(drop *types.Drop) []string {
-			return []string{
-				strconv.Itoa(drop.ItemID),
-				drop.DropType,
-			}
-		}).
-		ToSlice(&tuples)
-	if err != nil {
-		return err
-	}
-
 	itemDropInfos, typeDropInfos, err := d.DropInfoRepo.GetForCurrentTimeRangeWithDropTypes(ctx, &repos.DropInfoQuery{
 		Server:     reportTask.Server,
 		ArkStageId: report.StageID,
-		DropTuples: tuples,
 	})
 	if err != nil {
 		return err
-	}
-
-	if len(itemDropInfos) != len(drops) {
-		return fmt.Errorf("invalid drop info count: expected %d, but got %d", len(drops), len(itemDropInfos))
 	}
 
 	if err = d.verifyDropType(report, typeDropInfos); err != nil {
@@ -66,6 +45,7 @@ func (d *DropVerifier) Verify(ctx context.Context, report *types.SingleReport, r
 }
 
 func (d *DropVerifier) verifyDropType(report *types.SingleReport, dropInfos []*models.DropInfo) error {
+	// dropTypeAmountMap: key is drop type, value is amount (how many kinds of items are dropped under this type)
 	dropTypeAmountMap := make(map[string]int)
 	linq.From(report.Drops).
 		SelectT(func(drop *types.Drop) string {
@@ -102,24 +82,47 @@ func (d *DropVerifier) verifyDropType(report *types.SingleReport, dropInfos []*m
 	return nil
 }
 
+/**
+ * Verify drop item quantity
+ * Check 1: iterate drops, check if any item is not in dropInfos
+ * Check 2: iterate dropInfos, check if quantity is within bounds
+ */
 func (d *DropVerifier) verifyDropItem(report *types.SingleReport, dropInfos []*models.DropInfo) error {
+	itemIdSetFromDropInfos := make(map[int]struct{})
 	for _, dropInfo := range dropInfos {
-		for _, drop := range report.Drops {
-			if drop.DropType != dropInfo.DropType || drop.ItemID != int(dropInfo.ItemID.Int64) {
-				continue
-			}
-			count := drop.Quantity
-			if dropInfo.Bounds.Lower > count {
-				return fmt.Errorf("drop item `%d`: expected at least %d, but got %d", drop.ItemID, dropInfo.Bounds.Lower, count)
-			} else if dropInfo.Bounds.Upper < count {
-				return fmt.Errorf("drop item `%d`: expected at most %d, but got %d", drop.ItemID, dropInfo.Bounds.Upper, count)
-			}
-			if dropInfo.Bounds.Exceptions != nil {
-				if linq.From(dropInfo.Bounds.Exceptions).AnyWithT(func(exception int) bool {
-					return exception == count
-				}) {
-					return fmt.Errorf("drop item `%d`: expected not to have %d", drop.ItemID, count)
-				}
+		itemIdSetFromDropInfos[int(dropInfo.ItemID.Int64)] = struct{}{}
+	}
+
+	// dropItemQuantityMap: key is item id, value is a sub map, key is drop type, value is quantity
+	dropItemQuantityMap := make(map[int]map[string]int)
+	for _, drop := range report.Drops {
+		// Check 1
+		if _, ok := itemIdSetFromDropInfos[int(drop.ItemID)]; !ok {
+			return fmt.Errorf("item id %d: not found in drop info", drop.ItemID)
+		}
+		if _, ok := dropItemQuantityMap[drop.ItemID]; !ok {
+			dropItemQuantityMap[drop.ItemID] = make(map[string]int)
+		}
+		dropItemQuantityMap[drop.ItemID][drop.DropType] += drop.Quantity
+	}
+
+	// Check 2
+	for _, dropInfo := range dropInfos {
+		itemId := int(dropInfo.ItemID.Int64)
+		quantity := 0
+		if quantityMap, ok := dropItemQuantityMap[itemId]; ok {
+			quantity = quantityMap[dropInfo.DropType]
+		}
+		if dropInfo.Bounds.Lower > quantity {
+			return fmt.Errorf("drop item `%d`: expected at least %d, but got %d", itemId, dropInfo.Bounds.Lower, quantity)
+		} else if dropInfo.Bounds.Upper < quantity {
+			return fmt.Errorf("drop item `%d`: expected at most %d, but got %d", itemId, dropInfo.Bounds.Upper, quantity)
+		}
+		if dropInfo.Bounds.Exceptions != nil {
+			if linq.From(dropInfo.Bounds.Exceptions).AnyWithT(func(exception int) bool {
+				return exception == quantity
+			}) {
+				return fmt.Errorf("drop item `%d`: expected not to have %d", itemId, quantity)
 			}
 		}
 	}
