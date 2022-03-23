7d1324bce7fd7a95438dc37c518adf0309084cb3
GalvinGao <me@galvingao.com>
2022-03-23 14:13:05 +0800

    feat: add health check endpoint

---
+++ internal/controllers/health.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"go.uber.org/fx"
+
+	"github.com/penguin-statistics/backend-next/internal/server/svr"
+	"github.com/penguin-statistics/backend-next/internal/service"
+)
+
+type HealthController struct {
+	fx.In
+
+	HealthService *service.HealthService
+}
+
+func RegisterHealthController(v3 *svr.V3, c HealthController) {
+	v3.Get("/health", cache.New(cache.Config{
+		// cache it for a second to mitigate potential DDoS
+		Expiration: time.Second,
+	}), c.Health)
+}
+
+func (c HealthController) Health(ctx *fiber.Ctx) error {
+	return c.HealthService.Ping(ctx.Context())
+}
