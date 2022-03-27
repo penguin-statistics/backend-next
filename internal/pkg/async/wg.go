8e3a186065c6ac283297e10c78b08355ce1ac580
GalvinGao <me@galvingao.com>
2022-03-27 22:43:23 +0800

    chore: atomically assign lastErr

--- internal/pkg/async/wg.go
+++ internal/pkg/async/wg.go
@@ -1,6 +1,9 @@
 package async
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // WaitAll waits for all the given errables to finish, and returns
 // the last error occurred in all errables, if any.
@@ -8,14 +11,13 @@ func WaitAll(chans ...<-chan error) error {
 	var wg sync.WaitGroup
 	wg.Add(len(chans))
 
-	var lastErr error
+	var lastErr atomic.Value
 	for _, ch := range chans {
 		go func(ch <-chan error) {
 			defer wg.Done()
 			if err, open := <-ch; open {
 				if err != nil {
-					// TODO: lastErr should be atomically updated
-					lastErr = err
+					lastErr.Store(err)
 				}
 			} else {
 				return
@@ -24,5 +26,5 @@ func WaitAll(chans ...<-chan error) error {
 	}
 
 	wg.Wait()
-	return lastErr
+	return lastErr.Load().(error)
 }
