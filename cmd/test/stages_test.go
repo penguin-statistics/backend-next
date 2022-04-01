b7a6cd236861715f9da636721c2b2ff4ae964a46
GalvinGao <me@galvingao.com>
2022-04-01 21:00:39 +0800

    test: remove unused stage test

--- cmd/test/stages_test.go
+++ cmd/test/stages_test.go
@@ -12,17 +12,6 @@ func BenchmarkV2Stages(b *testing.B) {
 	var app *fiber.App
 	populate(&app)
 
-	b.Run("GetsV2StagesWithoutRedisCache", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			resp, err := app.Test(httptest.NewRequest("GET", "/api/v3/stages/main_00-01?nocache=1", nil))
-			if err != nil {
-				b.Error(err)
-			}
-
-			assert.Equal(b, 200, resp.StatusCode, "expect success response")
-		}
-	})
-
 	b.Run("GetsV2StagesWithRedisCache", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			resp, err := app.Test(httptest.NewRequest("GET", "/api/v3/stages/main_00-01", nil))
