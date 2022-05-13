e10226e834f7b10c0c73cdb7028a3a23763c98f4
GalvinGao <me@galvingao.com>
2022-05-13 15:17:26 +0800

    fix: redundant patterns

--- internal/repo/drop_pattern.go
+++ internal/repo/drop_pattern.go
@@ -15,6 +15,7 @@ import (
 	"github.com/penguin-statistics/backend-next/internal/model"
 	"github.com/penguin-statistics/backend-next/internal/model/types"
 	"github.com/penguin-statistics/backend-next/internal/pkg/pgerr"
+	"github.com/penguin-statistics/backend-next/internal/util/reportutil"
 )
 
 type DropPattern struct {
@@ -85,6 +86,8 @@ func (s *DropPattern) GetOrCreateDropPatternFromDrops(ctx context.Context, tx bu
 }
 
 func (s *DropPattern) calculateDropPatternHash(drops []*types.Drop) (originalFingerprint, hexHash string) {
+	drops = reportutil.MergeDropsByItemID(drops)
+
 	segments := make([]string, len(drops))
 
 	for i, drop := range drops {
