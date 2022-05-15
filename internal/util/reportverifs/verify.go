aa94fb3738980e5fafd0dc43c3f8f5538575befd
GalvinGao <me@galvingao.com>
2022-05-15 19:11:26 +0800

    refactor: move common types

--- internal/util/reportverifs/verify.go
+++ internal/util/reportverifs/verify.go
@@ -8,26 +8,6 @@ import (
 	"github.com/penguin-statistics/backend-next/internal/pkg/observability"
 )
 
-type Violations map[int]*Violation
-
-func (v Violations) Reliability(index int) int {
-	if violation, ok := v[index]; ok {
-		return violation.Reliability
-	}
-
-	return 0
-}
-
-type Violation struct {
-	Rejection
-	Name string `json:"name"`
-}
-
-type Rejection struct {
-	Reliability int    `json:"reliability"`
-	Message     string `json:"message"`
-}
-
 type Verifier interface {
 	Name() string
 	Verify(ctx context.Context, report *types.ReportTaskSingleReport, reportTask *types.ReportTask) *Rejection
