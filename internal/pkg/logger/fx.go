f445c27e3a9d635f1454ec0f49fd21df7194649c
GalvinGao <me@galvingao.com>
2022-10-21 19:51:41 -0400

    fix: use info level for fx.init logs

--- internal/pkg/logger/fx.go
+++ internal/pkg/logger/fx.go
@@ -1,15 +1,37 @@
 package logger
 
 import (
+	"io"
+
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"go.uber.org/fx/fxevent"
 )
 
+type fxLogger struct {
+	l zerolog.Logger
+}
+
+var _ io.Writer = (*fxLogger)(nil)
+
 func Fx() fxevent.Logger {
-	return &fxevent.ConsoleLogger{
-		W: log.Logger.
+	logger := fxLogger{
+		l: log.Logger.
 			With().
 			Str("evt.name", "fx.init").
 			Logger(),
 	}
+	return &fxevent.ConsoleLogger{
+		W: logger,
+	}
+}
+
+func (l fxLogger) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if n > 0 && p[n-1] == '\n' {
+		// Trim CR added by stdlog.
+		p = p[0 : n-1]
+	}
+	l.l.Info().CallerSkipFrame(1).Msg(string(p))
+	return
 }
