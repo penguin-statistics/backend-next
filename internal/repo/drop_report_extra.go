8c066e9ee5ead7cb163ccb9c6423c92a5c8c362b
GalvinGao <me@galvingao.com>
2023-02-10 15:58:13 -0500

    refactor: use db selector

--- internal/repo/drop_report_extra.go
+++ internal/repo/drop_report_extra.go
@@ -18,16 +18,16 @@ func NewDropReportExtra(db *bun.DB) *DropReportExtra {
 	return &DropReportExtra{db: db, sel: selector.New[model.DropReportExtra](db)}
 }
 
-func (c *DropReportExtra) GetDropReportExtraById(ctx context.Context, id int) (*model.DropReportExtra, error) {
-	return c.sel.SelectOne(ctx, func(q *bun.SelectQuery) *bun.SelectQuery {
+func (r *DropReportExtra) GetDropReportExtraById(ctx context.Context, id int) (*model.DropReportExtra, error) {
+	return r.sel.SelectOne(ctx, func(q *bun.SelectQuery) *bun.SelectQuery {
 		return q.Where("report_id = ?", id)
 	})
 }
 
-func (c *DropReportExtra) IsDropReportExtraMD5Exist(ctx context.Context, md5 string) bool {
+func (r *DropReportExtra) IsDropReportExtraMD5Exist(ctx context.Context, md5 string) bool {
 	var dropReportExtra model.DropReportExtra
 
-	count, err := c.db.NewSelect().
+	count, err := r.db.NewSelect().
 		Model(&dropReportExtra).
 		Where("md5 = ?", md5).
 		Count(ctx)
@@ -38,7 +38,7 @@ func (c *DropReportExtra) IsDropReportExtraMD5Exist(ctx context.Context, md5 str
 	return count > 0
 }
 
-func (c *DropReportExtra) CreateDropReportExtra(ctx context.Context, tx bun.Tx, report *model.DropReportExtra) error {
+func (r *DropReportExtra) CreateDropReportExtra(ctx context.Context, tx bun.Tx, report *model.DropReportExtra) error {
 	_, err := tx.NewInsert().
 		Model(report).
 		Exec(ctx)
