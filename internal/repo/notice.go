542035922a52c488e10f2be698bf01bdff2442aa
GalvinGao <me@galvingao.com>
2023-02-10 15:59:33 -0500

    refactor: use db selector

--- internal/repo/notice.go
+++ internal/repo/notice.go
@@ -2,32 +2,24 @@ package repo
 
 import (
 	"context"
-	"database/sql"
 
-	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
 
 	"exusiai.dev/backend-next/internal/model"
+	"exusiai.dev/backend-next/internal/repo/selector"
 )
 
 type Notice struct {
-	DB *bun.DB
+	db  *bun.DB
+	sel selector.S[model.Notice]
 }
 
 func NewNotice(db *bun.DB) *Notice {
-	return &Notice{DB: db}
+	return &Notice{db: db, sel: selector.New[model.Notice](db)}
 }
 
-func (c *Notice) GetNotices(ctx context.Context) ([]*model.Notice, error) {
-	var notice []*model.Notice
-	err := c.DB.NewSelect().
-		Model(&notice).
-		Order("notice_id ASC").
-		Scan(ctx)
-
-	if err != nil && !errors.Is(err, sql.ErrNoRows) {
-		return nil, err
-	}
-
-	return notice, nil
+func (r *Notice) GetNotices(ctx context.Context) ([]*model.Notice, error) {
+	return r.sel.SelectMany(ctx, func(q *bun.SelectQuery) *bun.SelectQuery {
+		return q.Order("notice_id ASC")
+	})
 }
