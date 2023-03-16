834425a632db1579fc0373a6cad53e2f5573b184
AlvISsReimu <alvissreimu@gmail.com>
2023-03-15 23:12:56 -0700

    perf: improve global pattern matrix calc

--- internal/repo/pattern_matrix_element.go
+++ internal/repo/pattern_matrix_element.go
@@ -2,10 +2,7 @@ package repo
 
 import (
 	"context"
-	"database/sql"
-	"time"
 
-	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
 
 	"exusiai.dev/backend-next/internal/model"
@@ -27,68 +24,9 @@ func (s *PatternMatrixElement) BatchSaveElements(ctx context.Context, elements [
 	return nil
 }
 
-func (s *PatternMatrixElement) DeleteByServer(ctx context.Context, server string) error {
-	_, err := s.db.NewDelete().Model((*model.PatternMatrixElement)(nil)).Where("server = ?", server).Exec(ctx)
-	return err
-}
-
 func (s *PatternMatrixElement) DeleteByServerAndDayNum(ctx context.Context, server string, dayNum int) error {
 	_, err := s.db.NewDelete().Model((*model.PatternMatrixElement)(nil)).Where("server = ?", server).Where("day_num = ?", dayNum).Exec(ctx)
 	return err
-}
-
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategoryAndStartAndEndTimeAndStageIds(
-	ctx context.Context, server string, sourceCategory string, start *time.Time, end *time.Time, stageIds []int,
-) ([]*model.PatternMatrixElement, error) {
-	var elements []*model.PatternMatrixElement
-	startTimeStr := start.Format(time.RFC3339)
-	endTimeStr := end.Format(time.RFC3339)
-	err := s.db.NewSelect().Model(&elements).
-		Where("server = ?", server).
-		Where("source_category = ?", sourceCategory).
-		Where("start_time >= timestamp with time zone ?", startTimeStr).
-		Where("end_time <= timestamp with time zone ?", endTimeStr).
-		Where("stage_id IN (?)", bun.In(stageIds)).
-		Scan(ctx)
-	if errors.Is(err, sql.ErrNoRows) {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return elements, nil
-}
-
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategory(ctx context.Context, server string, sourceCategory string) ([]*model.PatternMatrixElement, error) {
-	var elements []*model.PatternMatrixElement
-	err := s.db.NewSelect().Model(&elements).Where("server = ?", server).Where("source_category = ?", sourceCategory).Scan(ctx)
-	if errors.Is(err, sql.ErrNoRows) {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return elements, nil
-}
-
-/**
- * @param startDayNum inclusive
- * @param endDayNum inclusive
- */
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategoryAndDayNumRange(
-	ctx context.Context, server string, sourceCategory string, startDayNum int, endDayNum int,
-) ([]*model.PatternMatrixElement, error) {
-	var elements []*model.PatternMatrixElement
-	err := s.db.NewSelect().Model(&elements).
-		Where("server = ?", server).
-		Where("source_category = ?", sourceCategory).
-		Where("day_num >= ?", startDayNum).
-		Where("day_num <= ?", endDayNum).
-		Scan(ctx)
-	if errors.Is(err, sql.ErrNoRows) {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return elements, nil
 }
 
 func (s *PatternMatrixElement) IsExistByServerAndDayNum(ctx context.Context, server string, dayNum int) (bool, error) {
@@ -97,4 +35,53 @@ func (s *PatternMatrixElement) IsExistByServerAndDayNum(ctx context.Context, ser
 		return false, err
 	}
 	return exists, nil
+}
+
+func (s *PatternMatrixElement) GetAllTimesForGlobalPatternMatrix(ctx context.Context, server string, sourceCategory string) ([]*model.AllTimesResultForGlobalPatternMatrix, error) {
+	subq2 := s.db.NewSelect().
+		TableExpr("pattern_matrix_elements").
+		Column("stage_id", "pattern_id", "times", "day_num").
+		Where("server = ?", server).
+		Where("source_category = ?", sourceCategory).
+		Where("times > 0")
+
+	subq1 := s.db.NewSelect().
+		TableExpr("(?) AS subq2", subq2).
+		Column("stage_id", "pattern_id", "times").
+		Group("stage_id", "pattern_id", "times", "day_num")
+
+	mainq := s.db.NewSelect().
+		TableExpr("(?) AS subq1", subq1).
+		Column("stage_id", "pattern_id").
+		ColumnExpr("SUM(times) AS times").
+		Group("stage_id", "pattern_id")
+
+	results := make([]*model.AllTimesResultForGlobalPatternMatrix, 0)
+	err := mainq.Scan(ctx, &results)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func (s *PatternMatrixElement) GetAllQuantitiesForGlobalPatternMatrix(ctx context.Context, server string, sourceCategory string) ([]*model.AllQuantitiesResultForGlobalPatternMatrix, error) {
+	subq1 := s.db.NewSelect().
+		TableExpr("pattern_matrix_elements").
+		Column("stage_id", "pattern_id", "quantity").
+		Where("server = ?", server).
+		Where("source_category = ?", sourceCategory).
+		Where("quantity > 0")
+
+	mainq := s.db.NewSelect().
+		TableExpr("(?) AS subq1", subq1).
+		Column("stage_id", "pattern_id").
+		ColumnExpr("SUM(quantity) AS quantity").
+		Group("stage_id", "pattern_id")
+
+	results := make([]*model.AllQuantitiesResultForGlobalPatternMatrix, 0)
+	err := mainq.Scan(ctx, &results)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
 }
--- internal/service/pattern_matrix_element.go
+++ internal/service/pattern_matrix_element.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"time"
 
 	"exusiai.dev/backend-next/internal/model"
 	"exusiai.dev/backend-next/internal/repo"
@@ -22,30 +21,40 @@ func (s *PatternMatrixElement) BatchSaveElements(ctx context.Context, elements [
 	return s.PatternMatrixElementRepo.BatchSaveElements(ctx, elements, server)
 }
 
-func (s *PatternMatrixElement) DeleteByServer(ctx context.Context, server string) error {
-	return s.PatternMatrixElementRepo.DeleteByServer(ctx, server)
-}
-
 func (s *PatternMatrixElement) DeleteByServerAndDayNum(ctx context.Context, server string, dayNum int) error {
 	return s.PatternMatrixElementRepo.DeleteByServerAndDayNum(ctx, server, dayNum)
 }
 
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategoryAndStartAndEndTimeAndStageIds(
-	ctx context.Context, server string, sourceCategory string, start *time.Time, end *time.Time, stageIds []int,
-) ([]*model.PatternMatrixElement, error) {
-	return s.PatternMatrixElementRepo.GetElementsByServerAndSourceCategoryAndStartAndEndTimeAndStageIds(ctx, server, sourceCategory, start, end, stageIds)
-}
-
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategory(ctx context.Context, server string, sourceCategory string) ([]*model.PatternMatrixElement, error) {
-	return s.PatternMatrixElementRepo.GetElementsByServerAndSourceCategory(ctx, server, sourceCategory)
-}
-
-func (s *PatternMatrixElement) GetElementsByServerAndSourceCategoryAndDayNumRange(
-	ctx context.Context, server string, sourceCategory string, startDayNum int, endDayNum int,
-) ([]*model.PatternMatrixElement, error) {
-	return s.PatternMatrixElementRepo.GetElementsByServerAndSourceCategoryAndDayNumRange(ctx, server, sourceCategory, startDayNum, endDayNum)
-}
-
 func (s *PatternMatrixElement) IsExistByServerAndDayNum(ctx context.Context, server string, dayNum int) (bool, error) {
 	return s.PatternMatrixElementRepo.IsExistByServerAndDayNum(ctx, server, dayNum)
+}
+
+func (s *PatternMatrixElement) GetAllTimesForGlobalPatternMatrixMapByStageIdAndItemId(ctx context.Context, server string, sourceCategory string) (map[int]map[int]*model.AllTimesResultForGlobalPatternMatrix, error) {
+	allTimes, err := s.PatternMatrixElementRepo.GetAllTimesForGlobalPatternMatrix(ctx, server, sourceCategory)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[int]map[int]*model.AllTimesResultForGlobalPatternMatrix)
+	for _, v := range allTimes {
+		if _, ok := result[v.StageID]; !ok {
+			result[v.StageID] = make(map[int]*model.AllTimesResultForGlobalPatternMatrix)
+		}
+		result[v.StageID][v.PatternID] = v
+	}
+	return result, nil
+}
+
+func (s *PatternMatrixElement) GetAllQuantitiesForGlobalPatternMatrixMapByStageIdAndItemId(ctx context.Context, server string, sourceCategory string) (map[int]map[int]*model.AllQuantitiesResultForGlobalPatternMatrix, error) {
+	allQuantities, err := s.PatternMatrixElementRepo.GetAllQuantitiesForGlobalPatternMatrix(ctx, server, sourceCategory)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[int]map[int]*model.AllQuantitiesResultForGlobalPatternMatrix)
+	for _, v := range allQuantities {
+		if _, ok := result[v.StageID]; !ok {
+			result[v.StageID] = make(map[int]*model.AllQuantitiesResultForGlobalPatternMatrix)
+		}
+		result[v.StageID][v.PatternID] = v
+	}
+	return result, nil
 }
