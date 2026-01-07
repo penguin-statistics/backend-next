70ff5782a877cc9b02fa1bf0d0504d3efd3660d7
GalvinGao <me@galvingao.com>
2026-01-07 13:21:20 +0900

    feat: add warn when body is invalid

--- internal/pkg/middlewares/rekuest.go
+++ internal/pkg/middlewares/rekuest.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"github.com/gofiber/fiber/v2"
 
+	"exusiai.dev/backend-next/internal/pkg/flog"
 	"exusiai.dev/backend-next/internal/pkg/pgerr"
 	"exusiai.dev/backend-next/internal/util/rekuest"
 )
@@ -11,10 +12,16 @@ func InjectValidBody[T any]() func(*fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		var dest T
 		if err := ctx.BodyParser(&dest); err != nil {
+			flog.WarnFrom(ctx, "middlewares.inject_valid_body.body_parser").
+				Err(err).
+				Msg("invalid request")
 			return pgerr.ErrInvalidReq.Msg("invalid request: %s", err)
 		}
 
 		if err := rekuest.ValidateStruct(ctx, dest); err != nil {
+			flog.WarnFrom(ctx, "middlewares.inject_valid_body.validate_struct").
+				Any("errors", err).
+				Msg("invalid request")
 			return pgerr.NewInvalidViolations(err)
 		}
 
